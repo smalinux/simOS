4baa74c75e33a4f7ab80a22bd2fb5729df7c6794
smalinux <sohaibfacebook396@gmail.com>
2020-03-28 12:28:45 +0200

    Loading FileTable into first sector, and 'kernel' into second sector!

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -3,6 +3,7 @@
 ;;;
 	[org 0x7c00]
 
+	;; READ FILE TABLE INTO MEMORY FIRST
 	;; set up ES:BX memory address/segment:offset 
 	mov bx, 0x1000				; loads sector to memory address 0x1000
 	mov es, bx 					; ES = 0x1000
@@ -14,22 +15,43 @@
 	mov ch, 0					; cylinder 0
 	mov cl, 0x02				; starting sector to read from disk
 
-read_disk:
+read_disk1:
 	mov ah, 0x02
 	mov al, 0x01
 	int 0x13					; BIOS read from disk int
 
-	jc read_disk				; retry if disk read error (CF = 1)
+	jc read_disk1				; retry if disk read error (CF = 1)
+
+	;; READ KERNEL INTO MEMORY SECOND
+
+	;; set up ES:BX memory address/segment:offset 
+	mov bx, 0x2000				; loads sector to memory address 0x2000
+	mov es, bx 					; ES = 0x1000
+	mov bx, 0x0					; ES:BX = 0x1000:0
+
+	;; set up DX register for disk loading
+	mov dh, 0					; head 0
+	mov dl, 0					; drive 0
+	mov ch, 0					; cylinder 0
+	mov cl, 0x03				; starting sector to read from disk
+
+read_disk2:
+	mov ah, 0x02
+	mov al, 0x01
+	int 0x13					; BIOS read from disk int
+
+	jc read_disk2				; retry if disk read error (CF = 1)
+
 
 	;; reset segment registers for RAM
-	mov ax, 0x1000
+	mov ax, 0x2000
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
 
-	jmp 0x1000:0 				; move instruction pointer to our kernel (fare jmp)
+	jmp 0x2000:0x0 				; move instruction pointer to our kernel (fare jmp)
 
 
 ;;; Boot Sector Magic Mumber  
--- src/asm/fileTable.asm
+++ src/asm/fileTable.asm
@@ -2,3 +2,6 @@
 ;;; Basic file table
 ;;;
 db '{calculator-04, program2-06}'
+	
+	;; sector padding magic!
+	times 512-($-$$) db 0
